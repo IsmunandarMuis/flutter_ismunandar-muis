ed72262179e018d18d4260d117dab6738bb7b85c
unknown <ismunandarmuis311@gmail.com>
2022-09-02 07:36:09 +0800

    section 8

--- 8_Advance Function, Async-Await/praktikum/nomor_1.dart
+++ 8_Advance Function, Async-Await/praktikum/nomor_1.dart
@@ -1,19 +1,18 @@
-void main(List<String> args) {
+void main(List<String> args) async {
   List<int> data = [4, 2, 4, 1, 3];
   int pengali = 2;
 
-  var hasil = perkalian(data: data, pengali: pengali);
+  var hasil = await perkalian(data: data, pengali: pengali);
 
-  for (int element in hasil) {
-    print(element);
-  }
+  print(hasil);
 }
 
 perkalian({required List<int> data, required int pengali}) async {
   List<int> newList = [];
-  Future.forEach(data, (int element) {
-    int hasilKali = element + pengali;
+  await Future.forEach(data, (int element) {
+    int hasilKali = element * pengali;
     newList.add(hasilKali);
   });
+
   return newList;
 }
