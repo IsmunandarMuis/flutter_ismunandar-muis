b65b67c9cbeb3a0c5c9f486048a5cf3b6efa7a6b
unknown <ismunandarmuis311@gmail.com>
2022-10-10 08:43:22 +0800

    section 23

--- 24_Storages/praktikum/bloc_state_management/lib/main.dart
+++ 24_Storages/praktikum/bloc_state_management/lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:bloc_state_management/bloc/auth_bloc.dart';
 import 'package:bloc_state_management/bloc/user_bloc.dart';
+import 'package:bloc_state_management/pages/home_page.dart';
 import 'package:bloc_state_management/pages/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString('email');
+  print(email);
+  runApp(MyApp(
+    checkLogin: (email != null) ? true : false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.checkLogin}) : super(key: key);
+
+  final bool checkLogin;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +28,9 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => AuthBloc()),
         BlocProvider(create: (_) => UserBloc()),
       ],
-      child: const MaterialApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: RegisterPage(),
+        home: checkLogin ? const HomePage() : const RegisterPage(),
       ),
     );
   }
--- 24_Storages/praktikum/provider_state_management/lib/main.dart
+++ 24_Storages/praktikum/provider_state_management/lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_state_management/pages/home_page.dart';
 import 'package:provider_state_management/pages/register_page.dart';
 import 'package:provider_state_management/providers/auth_proivder.dart';
 import 'package:provider_state_management/providers/user_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (_) => AuhtProvider(),
-    ),
-    ChangeNotifierProvider(
-      create: (_) => UserProvider(),
-    ),
-  ], child: const MyApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString('email');
+  print(email);
+  runApp(MyApp(
+    checkLogin: (email != null) ? true : false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.checkLogin}) : super(key: key);
 
+  final bool checkLogin;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuhtProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: checkLogin ? const HomePage() : const RegisterPage(),
+      ),
     );
   }
 }
